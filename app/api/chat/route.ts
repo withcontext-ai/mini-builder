0025b611befc92a830c34e9e17dfe2e6d10dd0fe
lizunlong <lizunlong@gmail.com>
2023-11-05 13:50:52 +0800

    use summary

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,11 +3,32 @@ import { kv } from '@vercel/kv'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import OpenAI from 'openai'
 
+import { getChat } from '@/lib/actions/chat'
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
 export const runtime = 'edge'
+
+const makeSummaryPrompt = (summary?: string) => {
+  return summary
+    ? [
+        {
+          role: 'system',
+          content: `This is a summary of the chat history as a recap: ${summary}`,
+        },
+      ]
+    : []
+}
+
+const summarizePrompt = [
+  {
+    role: 'system',
+    content:
+      'Summarize the discussion briefly in 200 words or less to use as a prompt for future context.',
+  },
+]
 
 export async function POST(req: NextRequest) {
   try {
@@ -21,40 +42,53 @@ export async function POST(req: NextRequest) {
       )
     }
 
+    const { summary } = await getChat(id)
+    const systemMessages = [
+      {
+        role: 'system',
+        content: 'You are ChatGPT, a large language model trained by OpenAI.',
+      },
+    ]
+    const summaryMessages = makeSummaryPrompt(summary)
+    const latestMessages = messages.slice(-5)
+
+    // Combine all messages
+    const _messages = [...systemMessages, ...summaryMessages, ...latestMessages]
+
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
       stream: true,
-      messages,
+      messages: _messages,
     })
 
     const stream = OpenAIStream(response, {
       async onCompletion(completion) {
-        const newMessages = [
-          ...messages,
+        const completionMessage = [
           {
             role: 'assistant',
             content: completion,
           },
         ]
-        const payload = {
+        const newMessages = [...messages, ...completionMessage]
+        await kv.hset(`chat:${id}`, {
           id,
           messages: newMessages,
-        }
-        await kv.hset(`chat:${id}`, payload)
-
-        const response = await openai.chat.completions.create({
-          model: 'gpt-3.5-turbo',
-          messages: [
-            ...newMessages,
-            {
-              role: 'system',
-              content:
-                'Summarize the discussion briefly in 200 words or less to use as a prompt for future context.',
-            },
-          ],
         })
-        const summary = response.choices[0].message.content
-        await kv.hset(`chat:${id}`, { summary })
+
+        // Summarize the chat history
+        if (newMessages.length > 5) {
+          const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+              ...summaryMessages,
+              ...latestMessages,
+              ...completionMessage,
+              ...summarizePrompt,
+            ],
+          })
+          const summary = response.choices[0].message.content
+          await kv.hset(`chat:${id}`, { summary })
+        }
       },
     })
 
